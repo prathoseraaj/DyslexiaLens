20582992183d46e5d4bc1484e83d7bd4c80166b4
prathoseraaj <prathoseraaj0312@gmail.com>
2025-08-04 10:33:40 +0530

    feat: navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      page    
-    </div>
-  )
-}
+    <div className="w-full h-[100vh] m-0 p-0">
+      <nav className="h-[7vh] flex justify-cente items-center ml-2">
+        <div className="flex flex-row gap-2">
+          <Image src='/svgviewer-output.svg' alt="logo" width={15} height={15} />
+          <h1 className="text-1xl font-bold">DyslexiaLens</h1>
+        </div>
+      </nav>
+      <hr className="text-gray-300" />
 
-export default page
+    </div>
+  );
+};
+
+export default page;
