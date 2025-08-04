deb93921460dc507c9607e47df4c74dc3337f179
prathoseraaj <prathoseraaj0312@gmail.com>
2025-08-04 11:15:39 +0530

    fix: prettier

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,19 +6,28 @@ const page = () => {
     <div className="w-full h-[100vh] m-0 p-0">
       <nav className="h-[7vh] flex  items-center ml-2">
         <div className="flex flex-row gap-2">
-          <Image src='/svgviewer-output.svg' alt="logo" width={15} height={15} />
+          <Image
+            src="/svgviewer-output.svg"
+            alt="logo"
+            width={15}
+            height={15}
+          />
           <h1 className="text-1xl font-bold">DyslexiaLens</h1>
         </div>
       </nav>
       <hr className="text-gray-300" />
 
-      <div className="h-[93h]"> 
+      <div className="h-[93h]">
         <div className="flex items-center m-10 mt-20 flex-col gap-1">
-          <h1 className="font-bold text-4xl font-serif"> DyslexiaLens - Text Simplifier </h1>
-          <p className="text-[12px] ">Paste or types your text below to simplify it for easier reading.</p>
+          <h1 className="font-bold text-4xl font-serif">
+            DyslexiaLens - Text Simplifier
+          </h1>
+          <p className="text-[12px] ">
+            Paste or types your text below to simplify it for easier reading.
+          </p>
 
           <div className="mt-15">
-            <textarea className="border border-gray-300 outline-0 resize-none rounded-[10px] w-[500px] h-[40vh]"/>
+            <textarea className="border border-gray-300 outline-0 resize-none rounded-[10px] w-[500px] h-[40vh]" />
           </div>
         </div>
       </div>
